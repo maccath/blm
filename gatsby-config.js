92aa5d864794c0166d9995e4ab9d7bd759af7fd9
Katy Ereira <katy@skillshare.com>
2020-06-05 16:48:34 +0100

    🎨 Style: Add Typography.js with Kirkham theme.

--- gatsby-config.js
+++ gatsby-config.js
@@ -31,5 +31,11 @@ module.exports = {
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
     `gatsby-plugin-styled-components`,
+    {
+      resolve: `gatsby-plugin-typography`,
+      options: {
+        pathToConfigModule: `src/utils/typography`,
+      },
+    },
   ],
 }
