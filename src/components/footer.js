a678ae5176f6af99c4c1d6063311a9557300cc8a
Katy Ereira <kachii@gmail.com>
2020-06-08 21:14:56 +0100

    ✨ Feat: Update footer (#17)
    * ✨ Feat: Improve footer contents.
    
    * 🚨 Test: Update test snapshots.

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,5 +1,6 @@
 import React from "react"
 import tw from "twin.macro"
+import { Github } from "@icons-pack/react-simple-icons"
 
 const Container = tw.footer`
   mt-10 text-sm text-center
@@ -7,12 +8,18 @@ const Container = tw.footer`
 
 export default function Footer() {
   return (
-    <Container>
-      <p>
-        Compiled by <a href="http://katy-ereira.co.uk">Katy Ereira</a> /{" "}
-        <a href="http://twitter.com/maccath">@maccath</a> | Fist icon by{" "}
-        <a href="https://thenounproject.com/cesarreynoso562/">Cesar Reynoso</a>
-      </p>
+    <Container className={"flex justify-center space-x-5"}>
+      <div>
+        <a href="https://thenounproject.com/cesarreynoso562/">
+          Fist icon by Cesar Reynoso
+        </a>
+      </div>
+      <div>
+        <a href="https://github.com/maccath/blm">
+          <Github size={15} className={"inline mr-2"} />
+          Contribute on GitHub
+        </a>
+      </div>
     </Container>
   )
 }
