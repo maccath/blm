ce972618522b319f9d0b226edfc12f95f74db551
Katy Ereira <katy@skillshare.com>
2020-06-09 17:27:57 +0100

    🚨 Test: Add test to demonstrate correct classes added to child components.

--- src/components/__tests__/ButtonBar.spec.js
+++ src/components/__tests__/ButtonBar.spec.js
@@ -1,12 +1,24 @@
 import React from "react"
 import renderer from "react-test-renderer"
 import "jest-styled-components"
+import { Link } from "gatsby"
 
 import ButtonBar from "../ButtonBar"
 
 describe("Button Bar", () => {
   it("renders correctly", () => {
     const tree = renderer.create(<ButtonBar />).toJSON()
+    expect(tree).toMatchSnapshot()
+  })
+
+  it("gives child elements the correct classes", () => {
+    const tree = renderer
+      .create(
+        <ButtonBar>
+          <Link to={"/test"}></Link>
+        </ButtonBar>
+      )
+      .toJSON()
     expect(tree).toMatchSnapshot()
   })
 })
